e3cae783868bcab384f5197e66eefbf3eb0c8898
laetitiabeles <laetitiabeles9@gmail.com>
2024-01-24 16:04:26 +0100

    fixing order of condition

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -6,11 +6,11 @@ last_d = abs(number) % 10
 if last_d < 0:
     last_d = -last_d
 
-if last_d > 5:
-    end_string = "is greater than 5"
-elif last_d == 0:
-    end_string = "is 0"
-elif last_d < 6 and last_d != 0:
-    end_string = "is less than 6 and not 0"
+if last_d == 0:
+    end_string = 'is 0'
+elif last_d > 5:
+    end_string = 'is greater than 5'
+else:
+    end_string = 'is less than 6 and not 0'
 
 print(f"Last digit of {number} is {last_d} and {end_string}")
