7541ec9e997af38e7fc27c64e5dfb8cbded93b51
laetitiabeles <laetitiabeles9@gmail.com>
2024-01-24 18:29:02 +0100

    fixing condition using ord()

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
     # Vérifie si c est compris entre a et z
-    if 'a' <= c <= 'z':
+    if ord('a') <= ord(c) <= ord('z'):
         return True
     else:
         return False
