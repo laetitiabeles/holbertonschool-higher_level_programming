119654b344058a81ebbb397d3ad288f1b2ca9a6e
laetitiabeles <laetitiabeles9@gmail.com>
2024-01-24 17:57:13 +0100

    add new line

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-for numbers in range(100):
+for numbers in range(99):
     print("{:02d}".format(numbers), end=", ")
-    if numbers == 99:
-        print(numbers)
+print("99\n")
