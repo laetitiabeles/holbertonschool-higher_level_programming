1943fac388b2acb57b6cf33482d8eef6698ccfd2
laetitiabeles <laetitiabeles9@gmail.com>
2024-01-24 17:49:06 +0100

    fixing print 99

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-for numbers in range(100):
+for numbers in range(99):
     if numbers < 10:
         print(f"0{numbers}, ", end="")
-    elif numbers == 99:
-        print(numbers)
     else:
         print(f"{numbers}, ", end="")
+
+print(99)
