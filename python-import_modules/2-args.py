0c9f215c0cd7cd172181cbba626d8423ae495cdc
laetitiabeles <laetitiabeles9@gmail.com>
2024-01-26 16:10:18 +0100

    add condition for args == 1

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,6 +5,8 @@ if __name__ == "__main__":
     args = len(sys.argv)
     if args == 2:
         print("{} argument:".format(args - 1), end="\n")
+    elif args == 1:
+        print("{} arguments.".format(args - 1), end="\n")
     else:
         print("{} arguments:".format(args - 1), end="\n")
 
