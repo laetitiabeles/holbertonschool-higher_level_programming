b590c3a839232b8a4d3f69499ef1da819462e861
laetitiabeles <laetitiabeles9@gmail.com>
2024-01-30 15:32:37 +0100

    fixing for loop

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 
 def print_list_integer(my_list=[]):
-    if my_list is None:
-        my_list = []
-
-    for index in my_list:
-        print("{:d}".format(index))
+    for index in range(len(my_list)):
+        print("{:d}".format(my_list[index]))
