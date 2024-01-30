baa02f583444f58fe3f8ffcb128268ff7bfaff96
laetitiabeles <laetitiabeles9@gmail.com>
2024-01-30 15:12:45 +0100

    function that replaces an element in list

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    if idx < 0:
+        return None
+
+    if idx > len(my_list):
+        return my_list
+
+    list_copy = my_list.copy()
+    list_copy[idx] = element
+    return list_copy
