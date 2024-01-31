5aa000c0d4f5c2b5e83aaee43fe8923c5c3d7ebf
laetitiabeles <laetitiabeles9@gmail.com>
2024-01-31 15:12:39 +0100

    fixing for loop with new list

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     new_list = my_list.copy()
-    for index in my_list:
+    for index in new_list:
         if index % 2 == 0:
             new_list[index] = True
         else:
