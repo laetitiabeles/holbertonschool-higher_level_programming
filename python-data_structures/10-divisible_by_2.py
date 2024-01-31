cd8b63ab5f57540de8f121e2858fb9a7fc7fd1f0
laetitiabeles <laetitiabeles9@gmail.com>
2024-01-31 15:17:30 +0100

    fixing condition in for loop

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 def divisible_by_2(my_list=[]):
     new_list = my_list.copy()
     for index in range(len(my_list)):
-        if index % 2 == 0:
+        if new_list[index] % 2 == 0:
             new_list[index] = True
         else:
             new_list[index] = False
