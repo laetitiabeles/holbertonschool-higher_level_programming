75b45dff2b48faec52b323c99113b26044bc9acf
laetitiabeles <laetitiabeles9@gmail.com>
2024-02-05 16:29:25 +0100

    square with size

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+
+class Square:
+    # init création d'une nouvelle instance de la classe
+    def __init__(self, size):
+        self.__size = size  # Attribut d'instance "size" (pv)
