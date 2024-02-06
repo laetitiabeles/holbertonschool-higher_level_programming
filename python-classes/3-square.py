fc9268a91242b1589ab166dac2cc55b62d4c8ead
laetitiabeles <laetitiabeles9@gmail.com>
2024-02-06 13:51:29 +0100

    area of a square task

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+
+"""class Square that defines a square"""
+
+
+class Square:
+
+    """ Square class that define a square with is size"""
+
+    def __init__(self, size=0):
+        if not type(size) is int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        """square size private instance attribute"""
+        self.__size = size
+
+    def area(self):
+        """method that returns the current square area"""
+        return self.__size ** 2
