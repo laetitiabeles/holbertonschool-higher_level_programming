6b7784910ea291729bcbb7de367314bcaf61a215
laetitiabeles <laetitiabeles9@gmail.com>
2024-02-06 15:13:46 +0100

    fixing conditions

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -31,6 +31,7 @@ class Square:
 
     @position.setter  # setter
     def position(self, value):
+        self.__position = value
         if (
             not isinstance(value, tuple)
             or len(value) != 2 or
@@ -39,7 +40,6 @@ class Square:
             or value[0] < 0 or value[1] < 0
         ):
             raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value
 
     def area(self):
         """method that returns the current square area"""
