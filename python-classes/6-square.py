59ee4e82d55136e98b90caed54c2830eab390952
laetitiabeles <laetitiabeles9@gmail.com>
2024-02-06 16:10:09 +0100

    fixing

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -22,7 +22,8 @@ class Square:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
+        else:
+            self.__size = value
 
     @property  # getter
     def position(self):
