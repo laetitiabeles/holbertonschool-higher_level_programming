58126f82dd7b8afa3450b1911aed83c7f4bfd738
laetitiabeles <laetitiabeles9@gmail.com>
2024-02-06 16:12:21 +0100

    fixing init

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -8,8 +8,8 @@ class Square:
     """ Square class that define a square with its size"""
 
     def __init__(self, size=0, position=(0, 0)):
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property  # getter
     def size(self):
