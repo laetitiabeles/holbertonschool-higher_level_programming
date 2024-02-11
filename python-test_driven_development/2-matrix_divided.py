99ec0b148c37c14a3f12fda7824cedfc6d157e0c
laetitiabeles <laetitiabeles9@gmail.com>
2024-02-11 18:37:50 +0100

    fixing first condition

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -24,13 +24,14 @@ def matrix_divided(matrix, div):
         new_matrix (list of lists):a new matrix w/ elts divided by div
     """
 
-    error_msg = "matrix must be a matrix (list of lists) of integers/floats"
 
-    if not all(
-                isinstance(row, list) and
-                all(isinstance(elem, (int, float)) for elem in row)
-                for row in matrix):
-        raise TypeError(error_msg)
+    if (
+        not isinstance(matrix, list) or matrix == [] or
+        not all(isinstance(row, list) for row in matrix) or
+        not all(isinstance(num, (int, float)) for row in matrix for num in row)
+    ):
+        raise TypeError("matrix must be a matrix (list of lists) "
+                        "of integers/floats")
 
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
