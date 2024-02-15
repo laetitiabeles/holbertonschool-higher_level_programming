bef51ebe0d5705e3b23b1023a0bf2bcd57a0fd55
laetitiabeles <laetitiabeles9@gmail.com>
2024-02-15 11:28:15 +0100

    fixing print

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -4,7 +4,7 @@
 Rectangle = __import__('9-rectangle').Rectangle
 
 
-class Square(BaseGeometry):
+class Square(Rectangle):
     """define a square
     Args:
         Rectangle (Rectangle): mother class
@@ -23,3 +23,10 @@ class Square(BaseGeometry):
             int: square area
         """
         return self.__size ** 2
+
+    def __str__(self):
+        """print rectangle width and height
+        Returns:
+            str: rectangle
+        """
+        return f"[{self.__class__.__name__}] {self.__width}/{self.__height}"
