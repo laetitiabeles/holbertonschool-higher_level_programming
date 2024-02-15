22c9772db653e79bc1f64fc057b475b383d512cb
laetitiabeles <laetitiabeles9@gmail.com>
2024-02-15 11:27:28 +0100

    task 11 square

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" class Square that inherits from BaseGeometry """
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(BaseGeometry):
+    """define a square
+    Args:
+        Rectangle (Rectangle): mother class
+    """
+    def __init__(self, size):
+        """init a square
+        Args:
+        size (int): square size
+        """
+        self.__size = size
+        self.integer_validator("size", size)
+
+    def area(self):
+        """give square area
+        Returns:
+            int: square area
+        """
+        return self.__size ** 2
