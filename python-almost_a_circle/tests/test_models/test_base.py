85a85c4d764dfbbe97631e586a10621e51190067
laetitiabeles <laetitiabeles9@gmail.com>
2024-02-21 12:02:41 +0100

    fixing setupclass to inherit tests

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -12,7 +12,7 @@ class testBase(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         """ setUpClass """
-        cls.base_instance = Base()
+        cls.test_instance = Base()
         """ Structure qui associe un value à un test """
         cls.id_values = {
             'test_id_none': id(None),
@@ -29,54 +29,54 @@ class testBase(unittest.TestCase):
     def test_id_none(self):
         """ id none """
         id_value = self.id_values[self._testMethodName]
-        self.base_instance.id = id_value
-        self.assertEqual(self.base_instance.id, id_value)
+        self.test_instance.id = id_value
+        self.assertEqual(self.test_instance.id, id_value)
 
     def test_id(self):
         """ id """
         id_value = self.id_values[self._testMethodName]
-        self.base_instance.id = id_value
-        self.assertEqual(self.base_instance.id, id_value)
+        self.test_instance.id = id_value
+        self.assertEqual(self.test_instance.id, id_value)
 
     def test_id_string(self):
         """ id string """
         id_value = self.id_values[self._testMethodName]
-        self.base_instance.id = id_value
-        self.assertEqual(self.base_instance.id, id_value)
+        self.test_instance.id = id_value
+        self.assertEqual(self.test_instance.id, id_value)
 
     def test_id_negative(self):
         """ id negative """
         id_value = self.id_values[self._testMethodName]
-        self.base_instance.id = id_value
-        self.assertEqual(self.base_instance.id, id_value)
+        self.test_instance.id = id_value
+        self.assertEqual(self.test_instance.id, id_value)
 
     def test_id_float(self):
         """ id float """
         id_value = self.id_values[self._testMethodName]
-        self.base_instance.id = id_value
-        self.assertEqual(self.base_instance.id, id_value)
+        self.test_instance.id = id_value
+        self.assertEqual(self.test_instance.id, id_value)
 
     def test_id_is_zero(self):
         """ id is zero """
         id_value = self.id_values[self._testMethodName]
-        self.base_instance.id = id_value
-        self.assertEqual(self.base_instance.id, id_value)
+        self.test_instance.id = id_value
+        self.assertEqual(self.test_instance.id, id_value)
 
     def test_id_list(self):
         """ id is a list """
         id_value = self.id_values[self._testMethodName]
-        self.base_instance.id = id_value
-        self.assertEqual(self.base_instance.id, id_value)
+        self.test_instance.id = id_value
+        self.assertEqual(self.test_instance.id, id_value)
 
     def test_id_dict(self):
         id_value = self.id_values[self._testMethodName]
-        self.base_instance.id = id_value
-        self.assertEqual(self.base_instance.id, id_value)
+        self.test_instance.id = id_value
+        self.assertEqual(self.test_instance.id, id_value)
 
     def test_id_tuple(self):
         id_value = self.id_values[self._testMethodName]
-        self.base_instance.id = id_value
-        self.assertEqual(self.base_instance.id, id_value)
+        self.test_instance.id = id_value
+        self.assertEqual(self.test_instance.id, id_value)
 
 if __name__ == '__main__':
     unittest.main()
--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -15,13 +15,13 @@ class testRectangle(testBase):
     def setUpClass(cls):
         """ setUpClass """
         super().setUpClass()
-        """ Crée une instance pour test_rectangle """
-        test_instance = cls()
         """ Obtient le nom de la méthode """
-        test_method_name = test_instance._testMethodName
+        test_method_name = cls.__name__
         """ Obtient la valeur associée au nom sinon valeur = 0 """
-        id_value = cls.id_values.get(test_method_name, 0)
-        cls.rectangle_instance = Rectangle(1, 2, 3, 4, id_value)
+        cls.id_value = cls.id_values.get(test_method_name, 0)
+        """ Crée une instance pour test_rectangle """
+        cls.test_instance = Rectangle(1, 2, 3, 4, cls.id_value)
+
 
     def test_rectangle_inherits_from_base(self):
         """ Testing inheritance """
@@ -29,21 +29,21 @@ class testRectangle(testBase):
 
     def test_width(self):
         """Testing the Rectangle width getter"""
-        self.assertEqual(self.rectangle_instance.width, 1)
+        self.assertEqual(self.test_instance.width, 1)
 
     def test_height(self):
         """Testing the Rectangle height getter"""
-        self.assertEqual(self.rectangle_instance.height, 2)
+        self.assertEqual(self.test_instance.height, 2)
 
     def test_x(self):
         """Testing Rectangle x getter and setter"""
-        self.rectangle_instance.x = 54
-        self.assertEqual(self.rectangle_instance.x, 54)
+        self.test_instance.x = 54
+        self.assertEqual(self.test_instance.x, 54)
 
     def test_y(self):
         """Testing Rectangle y getter and setter"""
-        self.rectangle_instance.y = 45
-        self.assertEqual(self.rectangle_instance.y, 45)
+        self.test_instance.y = 45
+        self.assertEqual(self.test_instance.y, 45)
 
 if __name__ == '__main__':
     unittest.main()
