04136986d84b2b5ace6ec53886c7d0710c03a734
laetitiabeles <laetitiabeles9@gmail.com>
2024-02-21 14:32:36 +0100

    calmeeee

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -1,73 +1,76 @@
 #!/usr/bin/python3
-""" Unittest for Base class """
 
-import unittest
-import os
-import json
-
-
+import unittest, json
 from models.base import Base
-from models.rectangle import Rectangle
+
+""" Create a test class for the Base class """
 
 
-class TestBase(unittest.TestCase):
-    """ Testing instantiation of Base class """
+class testBase(unittest.TestCase):
+    """ Testing base """
+
+    @classmethod
+    def setUpClass(cls):
+        """ setUpClass """
+        cls.test_instance = Base()
 
     def test_no_instance_creation(self):
-        base1 = Base()
-        base2 = Base()
-        self.assertEqual(base1.id, base2.id - 1)
-
-    def test_instance_creation_with_id(self):
-        base1 = Base(10)
-        self.assertEqual(base1.id, 10)
+        self.test_instance = Base()
+        test_instance2 = Base()
+        self.assertEqual(self.test_instance.id, test_instance2.id - 1)
 
     def test_more_creation(self):
-        base1 = Base(11)
-        base2 = Base(42)
-        base3 = Base(1337)
-        self.assertEqual(base1.id, 11)
-        self.assertEqual(base2.id, 42)
-        self.assertEqual(base3.id, 1337)
+        self.test_instance = Base(11)
+        test2 = Base(42)
+        test3 = Base(1337)
+        self.assertEqual(self.test_instance.id, 11)
+        self.assertEqual(test2.id, 42)
+        self.assertEqual(test3.id, 1337)
 
     def test_None_id(self):
-        base1 = Base(None)
-        base2 = Base(None)
-        self.assertEqual(base1.id, base2.id - 1)
+        self.test_instance = Base(None)
+        test2 = Base(None)
+        self.assertEqual(self.test_instance.id, test2.id - 1)
 
-    def test_str_id(self):
-        self.assertEqual("hello", Base("hello").id)
+    def test_automatically_assigned_id(self):
+        """ Automatically assigned id """
+        self.test_instance = Base(20)
+        self.assertEqual(self.test_instance.id, 20)
 
-    def test_nb_instances_after_unique_id(self):
-        b1 = Base()
-        b2 = Base(12)
-        b3 = Base()
-        self.assertEqual(b1.id, b3.id - 1)
+    def test_id_string(self):
+        """ id string """
+        self.test_instance = Base("string")
+        self.assertEqual(self.test_instance.id, "string")
 
-    def test_negative_id(self):
-        self.assertEqual(-5, Base(-5).id)
+    def test_id_negative(self):
+        """ id negative """
+        self.test_instance = Base(-10)
+        self.assertEqual(self.test_instance.id, -10)
 
-    def test_float_id(self):
-        self.assertEqual(3.14, Base(3.14).id)
+    def test_id_float(self):
+        """ id float """
+        self.test_instance = Base(1.5)
+        self.assertEqual(self.test_instance.id, 1.5)
 
-    def test_zero_id(self):
-        self.assertEqual(0, Base(0).id)
+    def test_id_is_zero(self):
+        """ id is zero """
+        self.test_instance = Base(0)
+        self.assertEqual(self.test_instance.id, 0)
 
-    def test_dict_id(self):
-        self.assertEqual({"a": 1, "b": 2}, Base({"a": 1, "b": 2}).id)
+    def test_id_list(self):
+        """ id is a list """
+        self.test_instance = Base([1, 2, 3])
+        self.assertEqual(self.test_instance.id, [1, 2, 3])
 
-    def test_list_id(self):
-        self.assertEqual([1, 2, 3], Base([1, 2, 3]).id)
+    def test_id_dict(self):
+        """ id is a dictionary """
+        self.test_instance = Base({"a": 1, "b": 2})
+        self.assertEqual(self.test_instance.id, {"a": 1, "b": 2})
 
-    def test_tuple_id(self):
-        self.assertEqual((1, 2), Base((1, 2)).id)
-
-    def test_inf_id(self):
-        self.assertEqual(float('inf'), Base(float('inf')).id)
-
-    def test_NaN_id(self):
-        self.assertNotEqual(float('nan'), Base(float('nan')).id)
-
+    def test_id_tuple(self):
+        """ id is a tuple """
+        self.test_instance = Base((1,))
+        self.assertEqual(self.test_instance.id, (1,))
 
 if __name__ == '__main__':
     unittest.main()
